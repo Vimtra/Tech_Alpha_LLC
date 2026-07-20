ba05e82795b139b4bb548e8fb499f7ecb2d291c5
Vimtra <Vimtra@vimtra.com>
2026-07-21 02:33:07 +0530

    Refactor NodeIcon component to use a simple circular placeholder

--- react-site/src/components/home/hero/NodeIcon.jsx
+++ react-site/src/components/home/hero/NodeIcon.jsx
@@ -1,67 +1,11 @@
-import { Cloud, Cpu, GitMerge, Shield, Database, BarChart2, Settings } from 'lucide-react'
-
-function NodeIcon({ name }) {
-  const common = { size: 18, strokeWidth: 1.6, color: '#DCE6F5' }
-  switch (name) {
-    case 'brain':
-      return (
-        <g>
-          <g transform="translate(-9 -9)">
-            <Cpu {...common} />
-          </g>
-        </g>
-      )
-    case 'aws':
-      return (
-        <g>
-          <g transform="translate(-9 -9)">
-            <Cloud {...common} />
-          </g>
-        </g>
-      )
-    case 'infinity':
-      return (
-        <g>
-          <g transform="translate(-9 -9)">
-            <GitMerge {...common} />
-          </g>
-        </g>
-      )
-    case 'shield':
-      return (
-        <g>
-          <g transform="translate(-9 -9)">
-            <Shield {...common} />
-          </g>
-        </g>
-      )
-    case 'database':
-      return (
-        <g>
-          <g transform="translate(-9 -9)">
-            <Database {...common} />
-          </g>
-        </g>
-      )
-    case 'chart':
-      return (
-        <g>
-          <g transform="translate(-9 -9)">
-            <BarChart2 {...common} />
-          </g>
-        </g>
-      )
-    case 'gear':
-      return (
-        <g>
-          <g transform="translate(-9 -9)">
-            <Settings {...common} />
-          </g>
-        </g>
-      )
-    default:
-      return null
-  }
+function NodeIcon() {
+  // simple circular placeholder for node icon
+  return (
+    <g>
+      <circle cx="0" cy="0" r="6" fill="rgba(255,255,255,0.08)" />
+      <circle cx="0" cy="0" r="3" fill="rgba(255,255,255,0.18)" />
+    </g>
+  )
 }
 
 export default NodeIcon
